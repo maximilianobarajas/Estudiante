f2bdc3015388cf021f551e6221dd56a78c8774cc
Maximiliano Barajas Sánchez <111471026+maxbarajas@users.noreply.github.com>
2022-09-24 21:09:54 -0500

    Add files via upload

---
+++ estudiante.go
@@ -0,0 +1,36 @@
+
+// Golang program to illustrate the
+// concept of custom types
+package main
+  
+import (
+    "fmt"
+)
+  
+//Declaramos la estructura
+type Estudiante struct{
+      
+    // defining struct variables
+    nombre string
+    apellido string
+    edad int
+    carrera string
+}
+  
+//Funcion de descripción
+func (estudiante Estudiante) imprimir_detalles(){
+  
+    fmt.Printf("Nombre completo: %s %s", estudiante.nombre, estudiante.apellido)
+    fmt.Printf("\nCarrera: %s",estudiante.carrera)
+    fmt.Printf("\nEdad: %d \n", estudiante.edad)
+}
+
+func main() {
+    //Instanciamos la estructura
+    estudiante1 := Estudiante{"Alan", "Perez", 22,"Lic. en Derecho"}
+      
+    //Imprimimos la estructura
+    estudiante1.imprimir_detalles()
+}
+
+
